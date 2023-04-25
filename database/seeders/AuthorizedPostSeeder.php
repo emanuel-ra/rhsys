6ebafccb07a99e8b7691e40e4d2f4bb9819f2c81
Emanuel Ramirez Abarca <85582567+emanuel-ra@users.noreply.github.com>
2023-04-25 11:23:35 -0600

    Seeder authorized post enable and edited 2

--- database/seeders/AuthorizedPostSeeder.php
+++ database/seeders/AuthorizedPostSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\JopPosition;
 use App\Models\AuthorizedPost;
-use App\Models\branch;
+use App\Models\Branch;
 
 class AuthorizedPostSeeder extends Seeder
 {
