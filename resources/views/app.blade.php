a81ec6f30498b5de6cd3adde841a41696ffed553
Emanuel Ramirez Abarca <85582567+emanuel-ra@users.noreply.github.com>
2023-10-23 17:17:07 -0600

    ..

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,12 +8,12 @@
 @yield('body')
 
 @section('css')
-    <link rel="stylesheet" href="{{asset('public/css/app.css')}}">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
 @stop
 
 @section('js')
 
-    <script src="{{ asset('public/js/app.js') }}"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
     {{--
     <script src="{{ asset('js/main.js') }}"></script>
     --}}
